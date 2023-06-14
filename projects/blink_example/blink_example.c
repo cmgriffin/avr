2ad688f5a9f53ee9ebf8458e957176a24a15cd78
cmgriffin <cmg9329@gmail.com>
2023-06-13 20:54:18 -0400

    library cleanup

--- projects/blink_example/blink_example.c
+++ projects/blink_example/blink_example.c
@@ -1,14 +1,25 @@
 #include "global.h"
+#include <debug_minimal.h>
 #include <gpio.h>
+#include <uart.h>
 #include <util/delay.h>
 
 int main(void)
 {
+    UART_init();
     GPIO_setOutput(&(GPIO_TypeDef)GPIO_D13);
+    DEBUG_printstr("led pin set to output\n");
+
+    uint16_t ntimes = 0;
 
     for (;;)
     {
         GPIO_toggleValue(&(GPIO_TypeDef)GPIO_D13);
+        ntimes++;
+
+        DEBUG_printstr("led toggled: ");
+        DEBUG_printnum(ntimes);
+        DEBUG_printstr("times\n");
         _delay_ms(1000);
     }
 
--- projects/blink_example/global.h
+++ projects/blink_example/global.h
@@ -3,9 +3,11 @@
 
 // Global Defines (used by many avr-libc libaries)
 
-#define F_CPU 16000000L // if using a platformio board definition this would already be defined
+#define F_CPU \
+    16000000L // if using a platformio board definition this would already be
+              // defined
 
 // Debugging
-#define DEBUG // enable debugging
+// #define DEBUG // enable debugging
 
 #endif
