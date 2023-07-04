4302a4db0789fb1c8094ef1b2fb549cb16a53b75
cmgriffin <cmg9329@gmail.com>
2023-07-04 10:27:32 -0400

    changed debug minimal to use itoa

--- lib/debug_minimal.h
+++ lib/debug_minimal.h
@@ -5,11 +5,16 @@
 #include "global.h"
 #include "uart.h"
 #include <avr/pgmspace.h>
+#include <stdlib.h>
 
 #ifdef DEBUG
-#define DEBUG_print(s)    UART_printStr_p(PSTR(s))
-#define DEBUG_println(s)  UART_printStr_p(PSTR(s "\n"))
-#define DEBUG_printnum(i) UART_print_i16((int16_t)i, 0, false)
+#define DEBUG_print(s)   UART_printStr_p(PSTR(s))
+#define DEBUG_println(s) UART_printStr_p(PSTR(s "\n"))
+static inline void DEBUG_printnum(int16_t i)
+{
+    char s[10];
+    UART_printStr(itoa(i, s, 10));
+}
 #else
 #define DEBUG_print(s)
 #define DEBUG_println(s)
