409341e22503fa6b41a58c1af197c4285669731d
cgriffin <cgriffin@vicr.com>
2023-07-18 12:30:59 -0400

    debug minimal printint and printlong added

--- lib/debug_minimal.h
+++ lib/debug_minimal.h
@@ -10,14 +10,22 @@
 #ifdef DEBUG
 #define DEBUG_print(s)   UART_printStr_p(PSTR(s))
 #define DEBUG_println(s) UART_printStr_p(PSTR(s "\n"))
-static inline void DEBUG_printnum(int16_t i)
+static inline void DEBUG_printint(int16_t i)
 {
-    char s[10];
+    char s[7];
     UART_printStr(itoa(i, s, 10));
 }
+static inline void DEBUG_printlong(int32_t l)
+{
+    char s[12];
+    UART_printStr(ltoa(i, s, 10));
+}
+#define DEBUG_printnum(i) DEBUG_printnum(i)
 #else
 #define DEBUG_print(s)
 #define DEBUG_println(s)
 #define DEBUG_printnum(i)
+#define DEBUG_printint(i)
+#define DEBUG_printlong(i)
 #endif
 #endif /* DEBUG_MINIMAL_H */
